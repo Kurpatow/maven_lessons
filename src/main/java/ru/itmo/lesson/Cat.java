a665b5c0785ccd62aac70bcd18c045a15f400652
Nikita Kurpatow <kurpatowph@yandex.ru>
2022-12-12 21:01:54 +0300

    maven-lesson

---
+++ src/main/java/ru/itmo/lesson/Cat.java
@@ -0,0 +1,18 @@
+package ru.itmo.lesson;
+
+import lombok.*;
+
+@Getter
+@ToString(exclude = "name")
+// @ToString(exclude = {"name", "age"})
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Cat {
+    @Setter
+    @NonNull
+    private String name;
+
+    private String color;
+    @Setter
+    private int age;
+}
